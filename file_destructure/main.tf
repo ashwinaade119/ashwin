4117e55aa0c54b2bf6b01c5f4c08bb69855f54a3
ashu <ashwinaade1119@gmail.com>
2024-08-24 19:33:16 +0530

    --- Auto Git Commit ---

--- file_destructure/main.tf
+++ file_destructure/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "this" {
   ami                     = var.ami
   instance_type           = var.instance_type
   key_name                = var.key_name
-  user_data = file(var.user_data_file)
+  user_data               = 0var.user_data_file)
   tags = var.tags
 
 }
